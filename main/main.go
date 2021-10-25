8952f93da26a7dc4f46656bbf4512b4f128e7152
IshaanPota <ishaan.pota@gmail.com>
2021-10-25 14:39:42 -0700

    Access all devices on network

--- main/main.go
+++ main/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/gopacket/pcap"
+)
 
 func main() {
 
-	fmt.Println("wtffff")
+	var devices []pcap.Interface
+
+	devices, err := pcap.FindAllDevs()
+
+	for _, device := range devices {
+		fmt.Println(device.Flags)
+	}
+
+	if err != nil {
+
+		fmt.Print(err)
+	}
+
 }
