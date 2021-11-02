07e9daaf7d577da582212eee399df5005f1dcaf7
IshaanPota <ishaan.pota@gmail.com>
2021-11-02 15:03:43 -0400

    opened device, reading packets

--- main/main.go
+++ main/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"main/sniffing"
+	"time"
 
 	"github.com/google/gopacket/pcap"
 )
@@ -13,8 +15,13 @@ func main() {
 	devices, err := pcap.FindAllDevs()
 
 	for _, device := range devices {
-		fmt.Println(device.Flags)
+		fmt.Println(device.Name)
+
 	}
+
+	name := devices[13].Name
+
+	sniffing.SniffPackets(name, 1024, true, time.Hour)
 
 	if err != nil {
 
